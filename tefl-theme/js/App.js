301b6426c271c91181b60c3f0165cf37a45d7651
epiiaddone <lowlandgiant@yahoo.com>
2021-08-17 12:36:21 +0100

    Add files via upload

---
+++ tefl-theme/js/App.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+import RegistrationFormAjax from './modules/RegistrationFormAjax';
+import VerifyFormAjax from './modules/VerifyFormAjax';
+import MailContent from './modules/MailContent';
+
+var registrationFormAjax = new RegistrationFormAjax();
+var verifyFormAjax = new VerifyFormAjax();
+var mailContent = new MailContent();
